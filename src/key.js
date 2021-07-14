1f5b72748777d2a5acaf2899886f9b786616cd1c
saifhany <saifhany898@gmail.com>
2021-07-14 10:21:20 +0200

    s

--- src/key.js
+++ src/key.js
@@ -1,5 +1,5 @@
 module.exports= {
     MONGOURL: 'mongodb+srv://saifhany:minimini@cluster0.zj60m.mongodb.net/ecommerce?retryWrites=true&w=majority',
-    JWT_SECRET:'mernsecret'
+    JWT_SECRET:'mernsecret',
 
 }
